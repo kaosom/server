f9c526dc05e3efc7560c9002ecd96fe728b2c94d
= <=>
2024-06-13 09:11:27 -0600

    quitar node_modules

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,3 +6,5 @@ const router = express.Router();
 router.post('/twilio-webhook', twilioController.handleIncomingMessage);
 
 module.exports = router;
+
+//curl -X POST http://localhost:3000/twilio-webhook -d "From=%2B1234567890&Body=Hello%20Day"
