67fb00495a30c7e781be7e4d0baa290ed985cc12
= <=>
2024-06-13 14:29:07 -0600

    correccion de constantes

--- src/controllers/twilioController.js
+++ src/controllers/twilioController.js
@@ -6,8 +6,8 @@ const handleIncomingMessage = async (req, res) => {
     console.log('Solicitud entrante recibida en /twilio-webhook');
     console.log('Datos del cuerpo de la solicitud:', req.body);
 
-    const From = req.From;
-    const Body = req.Body; 
+    const From = req.body.From;
+    const Body = req.body.Body;
 
     // Registrar los valores extraídos
     console.log('From:', From);
